ad77547018f292bfbe2efd3329f29d169880f3ca
Zano <mikezano@gmail.com>
2025-06-07 10:08:26 -0700

    switch between x and o

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -5,6 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('tic-tac-toe initialized');
 });
 
+const Player = {
+    current: "X",
+    next() {
+        this.current = this.current === "X" ? "O" : "X";
+        return this.current;
+    }
+};
+
 document.getElementById('board').addEventListener('click',()=>{
     console.log('Board clicked');
 });
