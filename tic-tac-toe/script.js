29561f71067eb7ef4ebdb355190963217cc24e73
Zano <mikezano@gmail.com>
2025-06-07 10:05:45 -0700

    clicking board

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,4 +1,10 @@
 // JavaScript for tic-tac-toe
+
+let currentPlayer
 document.addEventListener('DOMContentLoaded', () => {
     console.log('tic-tac-toe initialized');
+});
+
+document.getElementById('board').addEventListener('click',()=>{
+    console.log('Board clicked');
 });
