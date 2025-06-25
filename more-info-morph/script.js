329d5aabb9991ad6bd408dd8b0c44fc974ba715d
Zano <mikezano@gmail.com>
2025-06-24 21:46:12 -0700

    copying from codepen

--- more-info-morph/script.js
+++ more-info-morph/script.js
@@ -1,4 +1,15 @@
-// JavaScript for more-info-morph
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('more-info-morph initialized');
+document.getElementById("red").addEventListener('click',async()=>{
+  const transition = document.startViewTransition(()=>{
+      console.log("red")
+      const el = document.getElementById("blue");
+      el.style.display = "block";
+  })
+  await transition.ready;
+})
+
+document.getElementById("blue").addEventListener('click', async(e)=>{
+  const transition = document.startViewTransition(()=>{
+    e.target.style.display = "none";
+  });
+  await transition.ready;
 });
