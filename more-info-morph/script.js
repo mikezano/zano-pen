4339e58e508d021a28469526f59ebde8bc30f242
Zano <mikezano@gmail.com>
2025-07-04 19:50:32 -0700

    clean up

--- more-info-morph/script.js
+++ more-info-morph/script.js
@@ -1,16 +1,10 @@
 document.getElementById("grid").addEventListener('click',async(el)=>{
   const transition = document.startViewTransition(()=>{
-
-      //const el = document.getElementById("blue");
-      //el.style.display = "block";
       if(el){
         const color = getComputedStyle(el.target).backgroundColor;
         const zoomEl = document.getElementById("zoom");
         zoomEl.style.backgroundColor = color;
         zoomEl.style.display = "block";
-        console.log(`color: ${color}, zoomEl: ${zoomEl}`);
-        console.log(zoomEl);
-        console.log(el.target.getBoundingClientRect());
       }
   })
   await transition.ready;
