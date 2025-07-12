98b24f42ebf0dc8d46c24e30fdf644861b9da93c
Zano <mikezano@gmail.com>
2025-07-12 16:51:20 -0700

    update for simon

--- simon/script.js
+++ simon/script.js
@@ -2,3 +2,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('simon initialized');
 });
+
+const colors = new Map(
+    [1, 'yellow'], 
+    [2, 'blue'],
+    [3, 'red'],
+    [4, 'green']
+);
+
